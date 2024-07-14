f826560a07320e4b80a0ecce1ff3dfb14829fadb
LuisRomero05 <lromeroi@unah.hn>
2024-07-13 20:14:56 -0600

    Creacion de datos y actualizacion
    Actualizacion de la ventana Welcome, creacion de archivos (ventanas) y de los botones

--- autenticacion/lib/screens/welcome_screen.dart
+++ autenticacion/lib/screens/welcome_screen.dart
@@ -1,3 +1,8 @@
+import 'package:autenticacion/screens/signin_screen.dart';
+import 'package:autenticacion/screens/signup_screen.dart';
+import 'package:autenticacion/theme/theme.dart';
+import 'package:autenticacion/widgets/custom_scaffold.dart';
+import 'package:autenticacion/widgets/welcome_button.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatelessWidget {
@@ -5,6 +10,69 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CustomScaffold(
+      child: Column(
+        children: [
+          Flexible(
+              flex: 8,
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 0,
+                  horizontal: 40.0,
+                ),
+                child: Center(
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: const TextSpan(
+                      children: [
+                        TextSpan(
+                            text: '¡Bienvenido de nuevo!\n',
+                            style: TextStyle(
+                              fontSize: 45.0,
+                              fontWeight: FontWeight.w600,
+                            )),
+                        TextSpan(
+                            text:
+                                '\nIngrese datos personales a su cuenta de empleado',
+                            style: TextStyle(
+                              fontSize: 20,
+                              // height: 0,
+                            ))
+                      ],
+                    ),
+                  ),
+                ),
+              )),
+          Flexible(
+            flex: 2,
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: Row(
+                children: [
+                  const Expanded(
+                    child: WelcomeButton(
+                      buttonText: 'Iniciar sesión',
+                      onTap: SignInScreen(),
+                      //Sign in, accion
+                      color: Colors.transparent,
+                      textColor: Colors.white,
+                    ),
+                  ),
+                  Expanded(
+                    child: WelcomeButton(
+                      buttonText: 'Registrarse',
+                      onTap:  const SignUpScreen(),
+                      //Sign up, accion
+                      color: Colors.white,
+                      textColor:  lightColorScheme.primary,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
